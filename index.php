5d2cc92e965f2d494da6fca1d1776a24dd2ad4b3
Paul Coral <paul.coral@epfl.ch>
2020-04-17 22:42:43 +0200

    homepage html

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>En cours</title>
+  </head>
+  <body>
+    <h1>Welcome</h1>
+    <p>This is a small Web interface for a RetroPi station.<p>
+    <h2>What do you want to do?</h2>
+    <p>
+      <ul>
+        <li><a href='/addGame/addGame.php'>Add a game to RetroPi</a></li>
+        <li>Coming Soon ...</li>
+      </ul>
+    </p>
+  </body>
+</html>
