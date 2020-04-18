9ee3cf62845514a19b43342db5f1c61297b3f9fa
Paul Coral <paulpatrick2009@hotmail.com>
2020-04-18 19:16:53 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,3 +30,55 @@ sudo chgrp $NEW_GROUP Variables -R
 sudo chmod 775 Variables -R
 
 
+
+################################
+#####     Server Setup     #####
+################################
+echo "#######################"
+echo "Starting : Server SETUP"
+
+path_is_ok=false
+RPIE_PATH_FILE='Variables/RPIE_FOLDER_PATH.php'
+RPIE_PATH=''
+
+while [ $path_is_ok = false ]
+do
+
+	while [ ! -d "$RPIE_PATH" ]
+	do
+		echo "Enter RetroPie directory path : "
+    	read RPIE_PATH
+		if [ ! -d "$RPIE_PATH" ]
+		then
+			echo "NOT A VALID DIRECTORY"
+		fi
+	done
+
+    echo "Is this the right path : $RPIE_PATH (y/[n])"
+    read response
+    if [ $response = 'y' ]
+	then
+		path_is_ok=true
+		touch $RPIE_PATH_FILE
+		> $RPIE_PATH_FILE
+		echo $RPIE_PATH >> $RPIE_PATH_FILE
+	fi
+done
+
+# Change privileges
+echo "Done, ajusting privileges"
+sudo chown $APACHE_USER $RPIE_PATH_FILE
+sudo chgrp $NEW_GROUP $RPIE_PATH_FILE
+sudo chmod 775 $RPIE_PATH_FILE
+
+
+################################
+			End
+################################
+
+
+echo ""
+echo "Finished, have fun :D"
+echo ""
+echo "Created by Paul Coral"
+echo "Pubished under GPL3 License, you are free to change or redistribute (but don't close sources)"
